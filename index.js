5b73ad19fad57ff5d47a9166514fe581af8f128d
YaoHuiJi <yaohj@hotmail.com>
2023-12-23 21:50:14 +0800

    reproduce issue

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
+import React, {Component} from 'react';
+import {AppRegistry, FlatList, Text} from 'react-native';
 import {name as appName} from './app.json';
+
+const Test = () => {
+  let data = [];
+
+  for (let i = 0; i < 50; i++) {
+    data.push({id: i});
+  }
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={item => item?.id}
+      renderItem={({item, index}) => {
+        console.log('Render:', index);
+        return <Text>{index}</Text>;
+      }}
+    />
+  );
+};
+
+class App extends Component {
+  render() {
+    return <Test />;
+  }
+}
 
 AppRegistry.registerComponent(appName, () => App);
